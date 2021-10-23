0c78d7d82f23c1071ccb5db7bd3f7451c0cf3214
yujie <158392613@qq.com>
2021-10-23 15:39:18 +0800

    fix: 删除无用依赖

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -2,7 +2,7 @@
 const importLocal = require('import-local')
 
 if (importLocal(__dirname)) {
-  require('npmlog').info('caee-cli', '正在使用caee-cli本地版本')
+  require('@caee/cli-utils-log').log.notice('cli', '正在使用caee-cli本地版本')
 } else {
   require('../lib').core(process.argv.slice(2))
 }
