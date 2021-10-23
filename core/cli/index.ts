47ad460ea3eaee40567029dafb620040af51ec39
yujie <158392613@qq.com>
2021-10-23 17:14:58 +0800

    feat: 用户权限自动降级功能开发

--- core/cli/index.ts
+++ core/cli/index.ts
@@ -1,5 +1,6 @@
 import semver from 'semver'
 import colors from 'colors/safe'
+import rootCheck from 'root-check'
 import { log } from '@caee/cli-utils-log'
 import pkg from './package.json'
 import { LOWEST_NODE_VERSION } from './const'
@@ -8,18 +9,32 @@ export function core() {
   try {
     checkPkgVersion()
     checkNodeVersion()
+    checkRoot()
   } catch (error) {
     log.error('cli', (error as Error).message)
   }
 }
 
+/**
+ * 检查 caee-cli 版本
+ */
 function checkPkgVersion() {
   log.notice('cli', `v${pkg.version}`)
 }
 
+/**
+ * 检查本地 node 版本是否符合要求
+ */
 function checkNodeVersion() {
   const currentNodeVersion = process.version
   if (semver.gt(LOWEST_NODE_VERSION, currentNodeVersion)) {
     throw new Error(colors.red(`caee-cli 需要安装 v${LOWEST_NODE_VERSION} 以上版本的 Node.js`))
   }
+}
+
+/**
+ * 降级用户权限
+ */
+function checkRoot() {
+  rootCheck()
 }
