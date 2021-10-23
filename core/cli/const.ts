c269e6f5a8dae44b90fe330c4b62f50bc52db162
yujie <158392613@qq.com>
2021-10-23 16:07:00 +0800

    feat: 完成检查node版本号功能

---
+++ core/cli/const.ts
@@ -0,0 +1,1 @@
+export const LOWEST_NODE_VERSION = '15.0.0'
--- core/cli/index.ts
+++ core/cli/index.ts
@@ -1,10 +1,25 @@
-import pkg from './package.json'
+import semver from 'semver'
+import colors from 'colors/safe'
 import { log } from '@caee/cli-utils-log'
+import pkg from './package.json'
+import { LOWEST_NODE_VERSION } from './const'
 
 export function core() {
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (error) {
+    log.error('cli', (error as Error).message)
+  }
 }
 
 function checkPkgVersion() {
   log.notice('cli', `v${pkg.version}`)
+}
+
+function checkNodeVersion() {
+  const currentNodeVersion = process.version
+  if (semver.gt(LOWEST_NODE_VERSION, currentNodeVersion)) {
+    throw new Error(colors.red(`caee-cli 需要安装 v${LOWEST_NODE_VERSION} 以上版本的 Node.js`))
+  }
 }
