a30168919a6e29a48d263639199fcc22867c527e
yujie <158392613@qq.com>
2021-10-29 17:59:28 +0800

    feat: 调通抽象类Command

--- command/init/src/index.ts
+++ command/init/src/index.ts
@@ -1,9 +1,27 @@
+import { Command } from '@caee/cli-models-command'
+
 interface InitOpts {
-  force: boolean
+  force?: boolean
 }
 
-export default function init(projectName: string, opts: InitOpts) {
-  console.log('进入init命令', projectName)
+type InitArgv = [projectName: string, opts: InitOpts]
+
+export class InitCommand extends Command<InitArgv> {
+  constructor(...argv: InitArgv) {
+    super(...argv)
+  }
+  init() {
+    throw new Error('Method not implemented.')
+  }
+  exec() {
+    throw new Error('Method not implemented.')
+  }
+}
+
+export default function init(...argv: InitArgv) {
+  const [projectName, opts] = argv
+  console.log('进入init命令aa', projectName)
   console.log('init command opts', opts)
   console.log('init command targetPath', process.env.CAEE_CLI_TARGET_PATH)
+  return new InitCommand(...argv)
 }
--- models/command/src/index.ts
+++ models/command/src/index.ts
@@ -1,3 +1,13 @@
 'use strict'
 
-export class Command {}
+export abstract class Command<T extends Array<any>> {
+  protected argv: T
+  constructor(...argv_: T) {
+    this.argv = argv_
+    console.log('command', 'constructor', argv_)
+  }
+
+  abstract init(): void
+
+  abstract exec(): void
+}
