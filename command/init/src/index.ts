01e571ff24925eca8223e289417fed67e96cc150
yujie <158392613@qq.com>
2021-10-29 09:28:55 +0800

    fix: test

--- command/init/src/index.ts
+++ command/init/src/index.ts
@@ -3,7 +3,7 @@ interface InitOpts {
 }
 
 export default function init(projectName: string, opts: InitOpts) {
-  console.log('init command projectName', projectName)
+  console.log('进入init命令', projectName)
   console.log('init command opts', opts)
   console.log('init command targetPath', process.env.CAEE_CLI_TARGET_PATH)
 }
