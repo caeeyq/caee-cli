8165742f4ef9b2e70626e6a83aae7dd0709d9bee
yujie <158392613@qq.com>
2021-10-30 17:04:30 +0800

    feat: 判断当前目录是否为空

--- command/init/src/index.ts
+++ command/init/src/index.ts
@@ -1,6 +1,8 @@
+import fs from 'fs'
+import { Command as Commander } from 'commander'
+
 import { Command } from '@caee/cli-models-command'
 import { log } from '@caee/cli-utils-log'
-import { Command as Commander } from 'commander'
 
 interface InitOpts {
   force?: boolean
@@ -24,7 +26,25 @@ export class InitCommand extends Command<InitArgv, InitOpts> {
   }
 
   exec() {
-    log.verbose('InitCommand exec', 'run exec')
+    this.prepare()
+  }
+
+  prepare() {
+    // 1. 判断当前目录是否为空
+    if (!this.isCwdEmpty()) {
+      // 1.1 询问是否继续创建
+      log.verbose('InitCommand', 'prepare', '当前目录不为空,询问是否继续创建')
+    } else {
+    }
+  }
+
+  isCwdEmpty() {
+    const cwdPath = process.cwd()
+    const fileList = fs.readdirSync(cwdPath).filter(fileName => {
+      const whiteList = ['node_modules']
+      return !fileName.startsWith('.') && !whiteList.includes(fileName)
+    })
+    return fileList.length <= 0
   }
 }
 
